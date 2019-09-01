5140207a1524f5d1cf5aa8598b89d3c700499820
Mandy Mericle <mandmericle@gmail.com>
2019-09-01 15:37:35 -0500

    Subquery slide

--- query_scripts/using_subqueries.py
+++ query_scripts/using_subqueries.py
@@ -2,19 +2,20 @@ from pydal import DAL, Field
 
 db = DAL("mysql://root:root@localhost/sakila?set_encoding=utf8mb4", fake_migrate_all=True)
 
-db.define_table('film', Field('title'), Field('description'), Field('rental_rate'))
+db.define_table('film', Field('film_id'), Field('title'), Field('description'), Field('rental_rate'))
 
 films = ["BENEATH RUSH", "CADDYSHACK JEDI", "REUNION WITCHES", "VOLCANO TEXAS", "SWARM GOLD"]
 
 
-def query_leaving_relational_space():
-    sub_query = db(db.film.rental_rate < 1.0)._select(
-        db.film.rental_rate,
-        groupby=db.film.rental_rate,
-        orderby=db.film.rental_rate,
-    )
+def using_a_list_of_ids():
+    film_ids = [
+        row.film_id for row in 
+        db(db.film.rental_rate < 1.0).select(
+            db.film.film_id,
+        )
+    ]
 
-    film_rows = db(db.film.rental_rate.belongs(sub_query)).select(
+    film_rows = db(db.film.rental_rate.belongs(film_ids)).select(
         db.film.title,
         db.film.description,
         db.film.rental_rate,
@@ -23,7 +24,7 @@ def query_leaving_relational_space():
     return film_rows
 
 
-def query_in_relational_space():
+def using_a_subquery():
     sub_query = db(db.film.rental_rate < 1.0)._select(
         db.film.rental_rate,
     )
@@ -42,8 +43,8 @@ if __name__ == '__main__':
     import timeit
 
     print(timeit.timeit(
-        "query_leaving_relational_space()", setup="from __main__ import query_leaving_relational_space", number=1000
+        "using_a_list_of_ids()", setup="from __main__ import using_a_list_of_ids", number=1000
     ))
     print(timeit.timeit(
-        "query_in_relational_space()", setup="from __main__ import query_in_relational_space", number=1000
+        "using_a_subquery()", setup="from __main__ import using_a_subquery", number=1000
     ))
